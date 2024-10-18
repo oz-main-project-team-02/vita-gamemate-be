b8c563746dc94fcc39541faed139dc5e2addb61e
moonyu <dbwls3681@naver.com>
2024-10-18 17:55:06 +0900

    🔨Refactor: 유저 생성시 지갑 연결, 테스트코드 수정

--- src/wallets/apps.py
+++ src/wallets/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class WalletsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "wallets"
+
+    def ready(self):
+        import wallets.signals
