17afe34eada9f5e33c0474a2586769f738dbf2d2
Rosaly-J <seohwi327@gmail.com>
2024-10-18 16:04:44 +0900

    ✨Feat: 특정 게임 조회
    - swagger 추가
    - 특정 게임 조회 api 기능 구현
    - 테스트 통과
    
    🔨Refactor: 게임 전체 조회
    - 게임 조회에서 전체 조회로 변경
    - sort 기능 삭제

--- src/games/models/game_model.py
+++ src/games/models/game_model.py
@@ -4,8 +4,8 @@ from django.db import models
 class Gametype(models.TextChoices):
     LOL = "리그오브레전드", "리그오브레전드"
     OVERWATCH = "오버워치", "오버워치"
-    # 게임 종류 추후 추가
-
+    TFT = "전략적팀전투", "전략적팀전투"
+    BG = "배틀그라운드", "배틀그라운드"
 
 class Game(models.Model):
     name = models.CharField(choices=Gametype.choices, max_length=255)
