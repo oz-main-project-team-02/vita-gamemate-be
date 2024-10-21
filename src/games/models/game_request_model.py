4edae64892c0aad819d0454163a0ec51c819c210
moonyu <dbwls3681@naver.com>
2024-10-21 18:33:56 +0900

    ✨Feat : 게임의뢰 모델 정의

---
+++ src/games/models/game_request_model.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from games.models import Game
+from users.models.user_model import User
+
+
+
+class GameRequest(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    mate = models.ForeignKey(User, related_name='mate_requests', on_delete=models.CASCADE)
+    price = models.IntegerField()
+    amount = models.IntegerField(default=1)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'game_request'
+
+
