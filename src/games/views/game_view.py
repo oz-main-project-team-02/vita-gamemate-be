75668e9d7cbd302bc49de9ff5c63cb58fa50a70f
Rosaly-J <seohwi327@gmail.com>
2024-10-21 16:07:04 +0900

    ✨Feat: 인기 게임 조회 구현
    - 인기 게임 조회 API 구현
    - view, urls, model, test 코드 작성
    - 테스트 통과 완료

--- src/games/views/game_view.py
+++ src/games/views/game_view.py
@@ -7,6 +7,7 @@ import games
 from games.models.game_model import Game
 from games.serializers.game_serializers import GameSerializer
 
+
 class GameListView(APIView):
     def get(self, request, *args, **kwargs):
         try:
@@ -17,6 +18,7 @@ class GameListView(APIView):
         except (ValueError, ValidationError, TypeError) as e:
             # 요청이 유효하지 않거나 데이터 조회에 실패한 경우
             return Response({"error": "게임 조회에 실패하였습니다."}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class GameDetailView(APIView):
     def get(self, request, game_id, *args, **kwargs):
@@ -29,6 +31,7 @@ class GameDetailView(APIView):
         except Game.DoesNotExist:
             # 게임이 존재하지 않는 경우, 400 응답 반환
             return Response({"error": "해당하는 게임을 찾지 못했습니다."}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class PopularGameListView(APIView):
     def get(self, request):
