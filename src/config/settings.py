dd83519330d97b9d2e8a3501c18ed6bf195839f3
hwangtate <ghkdxodud10253504@gmail.com>
2024-10-24 17:34:49 +0900

    🐛Fix: ci 해결

--- src/config/settings.py
+++ src/config/settings.py
@@ -207,7 +207,7 @@ DATABASES = {
     }
 }
 
-REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
+REDIS_HOST = os.environ.get("REDIS_HOST", "0.0.0.0")
 REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
 REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", "")
 
